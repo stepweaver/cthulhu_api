8b283f0eb891ac3bd34a349bcf4900b5da24af00
Stephen Weaver <sweaver1@ND.EDU>
2023-04-27 15:27:38 -0400

    Update index.js
    Implement morgan middleware

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
-const express = require("express");
+const express = require('express');
+  morgan = require('morgan');
+
 const app = express();
+
+// Middleware
+app.use(morgan('common')); // The 'common' parameter specifies that requests should be logged using Morgan's "common" format.
+
+app.get('/', (req, res) => {
+  res.send('Welcome to CthulhuFlix!');
+});
+
+app.get('/secreturl', (req, res) => {
+  res.send('This is a secret url with super top-secret content.');
+});
+
+app.listen(8080, () => {
+  console.log('Your app is listening on port 8080.');
+});
+
