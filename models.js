91e684bd2e9a0d7a51cdfc2f939a3ec3c2137d21
Stephen Weaver <sweaver1@ND.EDU>
2023-05-08 13:40:52 -0400

    Update models.js
    typo

--- models.js
+++ models.js
@@ -1,5 +1,5 @@
 const mongoose = require ('mongoose');
-  bycrypt = require('bcrypt');
+  bcrypt = require('bcrypt');
 
 let movieSchema = mongoose.Schema({
   Title: {type: String, required: true},
