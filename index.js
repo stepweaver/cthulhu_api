334f73230915fea2eed049f55af2d69bc8824198
Stephen Weaver <sweaver1@ND.EDU>
2023-05-09 13:59:20 -0400

    Update index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ const passport = require('passport');
 
 // Connect to the database
 mongoose.set('debug', true);
-mongoose.connect(process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect('mongodb://127.0.0.1:27017/cthulhuFlixDB', { useNewUrlParser: true, useUnifiedTopology: true });
 
 // Configure express-session middleware
 app.use(session({
