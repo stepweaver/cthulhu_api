c23159ee5f26ea5e5e5b8d5c8fb1b9d59adfb3d9
Stephen Weaver <sweaver1@ND.EDU>
2023-05-11 12:45:16 -0400

    typos

--- index.js
+++ index.js
@@ -72,7 +72,7 @@ app.post('/users',
     return res.status(422).json({ errors: errors.array() });
   }
 
-  let hashedPassword = Users.hashedPassword(req.body.Password);
+  let hashedPassword = Users.hashPassword(req.body.Password);
   Users.findOne({ Username: req.body.Username })
     .then((user) => {
       if (user) {
