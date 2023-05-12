6526ca5816848d21360cc99fd54b27810cf0f9e6
Stephen Weaver <sweaver1@ND.EDU>
2023-05-12 13:40:05 -0400

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,6 +1,6 @@
 const jwtSecret = 'your_jwt_secret'; // This has to be the same key used in the JWTStrategy
 
-const jwt = require('jsonwebtoken');
+const jwt = require('jsonwebtoken'),
   passport = require('passport');
   require('./passport.js');
 
