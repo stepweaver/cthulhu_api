3597f44f1e1b920034706ada0d61446d5d96665f
Stephen Weaver <sweaver1@ND.EDU>
2023-05-12 15:02:00 -0400

    Update auth.js
    implement promises

--- auth.js
+++ auth.js
@@ -1,7 +1,8 @@
 const jwtSecret = 'your_jwt_secret'; // This has to be the same key used in the JWTStrategy
 
-const jwt = require('jsonwebtoken'),
-  passport = require('passport');
+const jwt = require('jsonwebtoken');
+const passport = require('passport');
+const util = require('util');
 
 require('./passport.js');
 
@@ -11,25 +12,31 @@ let generateJWTToken = (user) => {
       expiresIn: '7d', // This specifies that the token will expire in 7 days
       algorithm: 'HS256' // This is the algorithm used to "sign" or encode the values of the JWT
     });
-  }
+  };
 
 /* POST login*/
 module.exports = (router) => {
-  router.post('/login', (req, res) => {
-    passport.authenticate('local', { session: false }, (error, user, info) => {
-      if (error || !user) {
+  router.post('/login', async (req, res) => {
+    try {
+      const [user, info] = await util.promisify(passport.authenticate('local', { session: false }))(req, res);
+      if (!user) {
         return res.status(400).json({
           message: 'Something is not right',
           user: user
         });
       }
-      req.login(user, { sessions: false }, (error) => {
+      req.login(user, { session: false }, async (error) => {
         if (error) {
           res.send(error);
         }
         let token = generateJWTToken(user.toJSON());
         return res.json({ user, token });
       });
-    }) (req, res);
+    } catch (error) {
+      return res.status(400).json({
+        message: 'Something is not right',
+        error: error.message
+      });
+    }
   });
-}
+};
