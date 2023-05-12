2d67a85a25eda67de8749ab9991d994eda054a16
Stephen Weaver <sweaver1@ND.EDU>
2023-05-12 15:37:51 -0400

    Update auth.js

--- auth.js
+++ auth.js
@@ -2,44 +2,46 @@ const jwtSecret = 'your_jwt_secret'; // This has to be the same key used in the 
 
 const jwt = require('jsonwebtoken');
 const passport = require('passport');
-const util = require('util');
-
-require('./passport.js');
+const require('./passport.js');
 
 let generateJWTToken = (user) => {
-  return jwt.sign(user, jwtSecret, {
+  return new Promise((resolve, reject) => {
+    jwt.sign(user, jwtSecret, {
       subject: user.Username, // This is the username you're encoding in the JWT
       expiresIn: '7d', // This specifies that the token will expire in 7 days
       algorithm: 'HS256' // This is the algorithm used to "sign" or encode the values of the JWT
+    }, (error, token) => {
+      if (error) {
+        reject(error);
+      } else {
+        resolve(token);
+      }
     });
-  };
+  });
+}
 
-/* POST login*/
+/* POST login */
 module.exports = (router) => {
   router.post('/login', (req, res) => {
-    return new Promise((resolve, reject) => {
-      passport.authenticate('local', { session: false }, async (err, user, info) => {
-        try {
-          if (err || !user) {
-            return reject({ status: 400, message: 'Something is not right', user: user });
-          }
-          req.login(user, { session: false }, async (error) => {
-            if (error) {
-              return reject({ status: 400, message: 'Something is not right', error: error.message });
-            }
-            let token = generateJWTToken(user.toJSON());
-            return resolve({ status: 200, data: { user, token } });
-          });
-        } catch (error) {
-          return reject({ status: 400, message: 'Something is not right', error: error.message });
+    passport.authenticate('local', { session: false }, (error, user, info) => {
+      if (error || !user) {
+        return res.status(400).json({
+          message: 'Something is not right',
+          user: user
+        });
+      }
+      req.login(user, { session: false }, (error) => {
+        if (error) {
+          res.send(error);
         }
-      })(req, res);
-    })
-      .then(response => {
-        return res.status(response.status).json(response.data);
-      })
-      .catch(error => {
-        return res.status(error.status).json({ message: error.message, error: error.error });
+        generateJWTToken(user.toJSON())
+          .then((token) => {
+            return res.json({ user, token });
+          })
+          .catch((error) => {
+            return res.send(error);
+          });
       });
+    })(req, res);
   });
-};
+}
