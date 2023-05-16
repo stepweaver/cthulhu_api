a8aeef6cb27e008e1b2b7db3ab1396757ae18a4b
Stephen Weaver <sweaver1@ND.EDU>
2023-05-16 14:45:54 -0400

    Setup log stream to log.txt

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const morgan = require('morgan');
+const fs = require('fs'); // import built in node modules fs and path
+const path = require('path');
 
 const app = express();
+// create a write stream (in append mode)
+// a 'log.txt' file is created in root directory
+const accessLogStream = fs.createWriteStream(path.join(__dirname, 'log.txt'), {flags: 'a'})
 
 // Middleware
-app.use(morgan('common')); // The 'common' parameter specifies that requests should be logged using Morgan's "common" format.
+app.use(morgan('dev', {stream: accessLogStream})); // The 'dev' parameter specifies that requests should be logged using Morgan's "dev" format.
 
 app.get('/', (req, res) => {
   res.send('Welcome to CthulhuFlix!');
