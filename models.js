17a1754818a1842c231c2074340132d129dfb7a1
Stephen Weaver <sweaver1@ND.EDU>
2023-06-19 14:29:36 -0400

    Create models.js

---
+++ models.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+
+let movieSchema = mongoose.Schema({
+  Title: {type: String, required: true},
+  Description: {type: String, required: true},
+  releaseYear: {type: String, required: true},
+  Rating: {type: String, require: true},
+  Genre: {
+    Name: String,
+    Description: String
+  },
+  Director: {
+    Name: String,
+    Bio: String,
+    yearOfBirth: String
+  },
+  Actors: [String],
+  ImagePath: String,
+  Featured: Boolean
+});
+
+let userSchema = mongoose.Schema({
+  Username: {type: String, required: true},
+  Password: {type: String, required: true},
+  Email: {type: String, required: true},
+  Birthday: Date,
+  FavoriteMovies: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Movie' }] // Rather than the FavoriteMovies key containing an array of IDs for movie documents, the FavoriteMovies key could contain an array of actual movie documents, all embedded with each user document.
+});
+
+let Movie = mongoose.model('Movie', movieSchema);
+let User = mongoose.model('User', userSchema);
+
+module.exports.Movie = Movie;
+module.exports.User = User;
