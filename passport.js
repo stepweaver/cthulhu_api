83e69bed943f46577100617f1d519737052a4ddf
Stephen Weaver <sweaver1@ND.EDU>
2023-06-26 08:31:46 -0400

    Update passport.js

--- passport.js
+++ passport.js
@@ -1,6 +1,6 @@
 const passport = require('passport'),
   LocalStrategy = require('passport-local').Strategy,
-  Models = require('./models'),
+  Models = require('./models.js'),
   passportJWT = require('passport-jwt');
 
 let Users = Models.User,
@@ -10,38 +10,33 @@ let Users = Models.User,
 passport.use(new LocalStrategy({
   usernameField: 'Username',
   passwordField: 'Password'
-}, (username, password) => {
-  console.log(username + ' ' + password);
-    
-  return new Promise((resolve, reject) => {
-    Users.findOne({ Username: username })
-      .then((user) => {
-        if (!user) {
-          console.log('incorrect username');
-          reject({ message: 'Incorrect username or password' });
-        }
-          
-        console.log('finished');
-        resolve(user);
-      })
-      .catch((error) => {
-        console.log(error);
-        reject(error);
-      });
+}, (username, password, callback) => {
+  console.log(username + '  ' + password);
+  Users.findOne({ Username: username }, (error, user) => {
+    if (error) {
+      console.log(error);
+      return callback(error);
+    }
+
+    if (!user) {
+      console.log('incorrect username');
+      return callback(null, false, {message: 'Incorrect username or password.'});
+    }
+
+    console.log('finished');
+    return callback(null, user);
   });
 }));
-  
+
 passport.use(new JWTStrategy({
   jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
   secretOrKey: 'your_jwt_secret'
-}, (jwtPayload) => {
-  return new Promise((resolve, reject) => {
-    Users.findById(jwtPayload._id)
-      .then((user) => {
-        resolve(user);
-      })
-      .catch((error) => {
-        reject(error);
-      });
-  });
+}, (jwtPayload, callback) => {
+  return Users.findById(jwtPayload._id)
+    .then((user) => {
+      return callback(null, user);
+    })
+    .catch((error) => {
+      return callback(error)
+    });
 }));
