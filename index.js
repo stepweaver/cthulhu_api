5ce8d8d8117d72972584eeb39f690d56611f9835
Stephen Weaver <sweaver1@ND.EDU>
2023-07-07 06:48:59 -0400

    Update index.js

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ const accessLogStream = fs.createWriteStream(path.join(__dirname, 'log.txt'), {f
 
 const { check, validationResult } = require('express-validator');
 
-let allowedOrigins = ['http://localhost:1234'];
+let allowedOrigins = ['*'];
 
 app.use(cors({
   origin: (origin, callback) => {
