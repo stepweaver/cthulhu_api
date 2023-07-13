51f3d3a31d2cbd2f0afd5e47cef9e09553628a6e
Stephen Weaver <sweaver1@ND.EDU>
2023-07-13 14:01:15 -0400

    Update index.js

--- index.js
+++ index.js
@@ -113,7 +113,7 @@ app.get('/documentation', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'documentation.html'));;
 });
 
-app.get('/movies', /*passport.authenticate('jwt', { session: false }),*/ (req, res) => {
+app.get('/movies', passport.authenticate('jwt', { session: false }), (req, res) => {
   Movies.find()
     .then((movies) => {
       res.status(200).json(movies);
